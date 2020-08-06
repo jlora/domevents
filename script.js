b3079f0c6f6b66064192d27792dff9a526918af7
Andres <jlora1983@gmail.com>
2020-08-05 22:18:39 -0600

    when click on the list item, it toggle the .done class on and off. Added buttons next to each list item to delete the item when clicked on its corresponding delete button. BONUS: When adding a new list item, it automatically adds the delete button next to it (hint: be sure to check if new items are clickable too

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
+var li = document.querySelectorAll("li");
+var removeButtons = document.getElementsByClassName("remove");
 
 function inputLength() {
 	return input.value.length;
 }
 
+
+
+function removeParent(event) {
+	event.target.removeEventListener("click", removeParent, false);
+	event.target.parentNode.remove();
+}
+
 function createListElement() {
 	var li = document.createElement("li");
+
+	var button = document.createElement("button");
+	button.innerHTML = "Remove";
+	button.onclick = removeParent;
+
+	li.addEventListener("click", toggleListItemAfterClick);
 	li.appendChild(document.createTextNode(input.value));
 	ul.appendChild(li);
+	li.appendChild(button);
 	input.value = "";
 }
 
@@ -25,6 +41,27 @@ function addListAfterKeypress(event) {
 	}
 }
 
+function toggleListItemAfterClick(event) {
+	let itemClass = event.target.getAttribute("class");
+	itemClass === "done" ? event.target.setAttribute("class", "") : event.target.setAttribute("class", "done");
+}
+
 button.addEventListener("click", addListAfterClick);
 
 input.addEventListener("keypress", addListAfterKeypress);
+
+function addEventListenerToListItems () {
+	var i;
+	for (i = 0; i < li.length; i++) {
+		li[i].addEventListener("click", toggleListItemAfterClick);
+	}
+}
+
+function addEventListenerToButtons () {
+	for (var i = 0; i < removeButtons.length; i++) {
+		removeButtons[i].addEventListener("click", removeParent, false);
+	}
+}
+
+addEventListenerToListItems();
+addEventListenerToButtons();
